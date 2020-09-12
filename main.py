6c293aa9f984eee17d7db041284751331371737c
Elisha Hollander <just4now666666@gmail.com>
2020-09-13 00:31:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def end(source, loops=1):
+    for i in range(len(source)):
+        loops += (source[i] == '[')-(source[i] == ']')
+        if not loops: return i+1
+def execute(source,i=0):
+    global pointer,data
+    while i < len(source):
+        pointer += (source[i] == '>') - (source[i] == '<');pointer %= 30000;data[pointer] += (source[i] == '+') - (source[i] == '-');print((source[i] == '.') * chr(data[pointer] % 256), end='')
+        if source[i] == ',': data[pointer] = ord(input('\n'))
+        if source[i] == '[': j = end(source[i + 1:]);execute_loop(source[i + 1:j + i]);i += j
+        i += 1
+    return data
+def execute_loop(source):
+    while execute(source)[pointer]%256:pass
+source=open('main.b','r').read();pointer=0;data=[0 for i in range(30000)];i=0;execute(source)
