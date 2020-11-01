d456bfc047ddecfed46d5db396b73fe05e32d8b0
Elisha Hollander <just4now666666@gmail.com>
2020-11-01 12:44:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='clibf',
     version='1.0.0',
     description='CLI tool to execute Brainfuck code from files and strings',
-    long_description=open('README.md').read(),
+    long_description=open('README.md').read().replace('my_bf_string', '"' + open('example.b').read() + '"'),
     long_description_content_type="text/markdown",
     url='https://github.com/donno2048/bf-interpreter',
     packages=find_packages(),
